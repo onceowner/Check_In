c64230bc32a69dd0eae278d5cd0a0e17054a27b9
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-21 01:23:18 +0800

    Update ikuuu.py

--- ikuuu.py
+++ ikuuu.py
@@ -1,7 +1,7 @@
 from util import *
 
-username = sys.argv[0] # 登录账号
-password = sys.argv[1] # 登录密码
+username = sys.argv[1] # 登录账号
+password = sys.argv[2] # 登录密码
 
 
 
@@ -13,9 +13,9 @@ def ikuuu():
         driver.find_element_by_xpath("//*[@id='password']").send_keys(password)
         driver.find_element_by_xpath("//*[@class='btn btn-primary btn-lg btn-block login']").click()
 
-        #time.sleep(5)
+        time.sleep(5)
 
-        driver.find_element_by_xpath("//*[@class='btn btn-primary']").click() 
+        driver.find_element_by_xpath("//*[@data-dismiss='modal']").click()
 
         driver.find_element_by_xpath("//*[@id='yesterday']").click()
         print('签到成功')
