bf14fddcdbdde2f4b7efbf360248bc0988260aaf
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-21 01:15:23 +0800

    Update shangwangke.py

--- shangwangke.py
+++ shangwangke.py
@@ -15,7 +15,7 @@ def shangwangke():
 
         #time.sleep(5)
 
-        #driver.find_element_by_xpath("//*[@class='btn btn-primary']").click() 
+        driver.find_element_by_xpath("//*[@data-dismiss='modal']").click() 
 
         driver.find_element_by_xpath("//*[@onclick='checkin()']").click()
         print('签到成功')
