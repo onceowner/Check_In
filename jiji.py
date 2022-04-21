0c1021ddc6b877046edf9d017506d736cf314487
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-22 00:13:35 +0800

    Update jiji.py

--- jiji.py
+++ jiji.py
@@ -9,7 +9,7 @@ def jiji():
     try:
         driver = get_web_driver()
         driver.get("https://a.luxury/signin")
-        print("加载成功")
+        #print("加载成功")
         #driver.find_element_by_xpath("//*[@class='nav-link dropdown-toggle nav-link-lg nav-link-user']").click()
         #driver.find_element_by_xpath("//*[@class='dropdown-item has-icon text-danger']").click()
         
@@ -19,16 +19,16 @@ def jiji():
         driver.find_element_by_xpath("//*[@name='email']").send_keys(username)
         driver.find_element_by_xpath("//*[@class='el-input__inner']").send_keys(password)
         driver.find_element_by_xpath("//*[@class='el-button el-button--primary']").click()
-        print("登录成功")
+        #print("登录成功")
         driver.get("https://a.luxury/user")
         driver.implicitly_wait(10)
         #time.sleep(5)
 
         driver.find_element_by_xpath("//*[@class='el-button el-button--primary']").click()
-        print("登录2成功")
-        time.sleep(5)
-        driver.find_element_by_xpath("//*[@class='ui button purple tiny mt-2 pio-tip mr-0 fontsize']").click()
-        print("签到成功")
+        #print("登录2成功")
+        #time.sleep(5)
+        driver.find_element_by_xpath("//*[@data-pio='每天签到 800MB~5200MB，祝君好运！1 级用户每月 2 号重置流量为 10G']").click()
+        #print("签到成功")
     except:
         raise
     finally:
