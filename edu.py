e8018a739b7f3820e4a317e7cb4e1c19f0a9efcc
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-21 10:56:54 +0800

    Update edu.py

--- edu.py
+++ edu.py
@@ -15,11 +15,11 @@ def edu():
         driver.find_element_by_xpath("//*[@id='email']").send_keys(username)
         driver.find_element_by_xpath("//*[@id='passwd']").send_keys(password)
         driver.find_element_by_xpath("//*[@id='login']").click()
-
+        print('登录成功')
         time.sleep(20)
-
+        driver.find_element_by_xpath("//*[@class='waves-attach waves-effect collapsed']").click()
         #driver.find_element_by_xpath("//*[@data-dismiss='modal']").click() 
-
+        print('我的成功')
         driver.find_element_by_xpath("//*[@id='checkin']").click()
         print('签到成功')
     except:
