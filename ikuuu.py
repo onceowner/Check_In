9278fb07ea72c086449e77173fe01fa8929f46a0
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-21 22:30:15 +0800

    Update ikuuu.py

--- ikuuu.py
+++ ikuuu.py
@@ -12,13 +12,16 @@ def ikuuu():
         
         driver.find_element_by_xpath("//*[@class='nav-link dropdown-toggle nav-link-lg nav-link-user']").click()
         driver.find_element_by_xpath("//*[@class='dropdown-item has-icon text-danger']").click()
-        time.sleep(5)
+        
+        driver.implicitly_wait(10)
+        #time.sleep(5)
         
         driver.find_element_by_xpath("//*[@id='email']").send_keys(username)
         driver.find_element_by_xpath("//*[@id='password']").send_keys(password)
         driver.find_element_by_xpath("//*[@class='btn btn-primary btn-lg btn-block login']").click()
 
-        time.sleep(5)
+        driver.implicitly_wait(10)
+        #time.sleep(5)
 
         driver.find_element_by_xpath("//*[@data-dismiss='modal']").click()
         driver.find_element_by_xpath("//*[@onclick='checkin()']").click()
