5d6061b21831275c4d22bdddf3c6f593984c155e
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-21 23:10:29 +0800

    Update ikuuu.py

--- ikuuu.py
+++ ikuuu.py
@@ -9,7 +9,7 @@ def ikuuu():
     try:
         driver = get_web_driver()
         driver.get("https://ikuuu.co/auth/login")
-        
+        #print("加载成功")
         #driver.find_element_by_xpath("//*[@class='nav-link dropdown-toggle nav-link-lg nav-link-user']").click()
         #driver.find_element_by_xpath("//*[@class='dropdown-item has-icon text-danger']").click()
         
@@ -19,14 +19,16 @@ def ikuuu():
         driver.find_element_by_xpath("//*[@id='email']").send_keys(username)
         driver.find_element_by_xpath("//*[@id='password']").send_keys(password)
         driver.find_element_by_xpath("//*[@class='btn btn-primary btn-lg btn-block login']").click()
-
-        #driver.get("https://ikuuu.co/user")
-        #driver.implicitly_wait(10)
+        #print("登录成功")
+        driver.get("https://ikuuu.co/user")
+        driver.implicitly_wait(10)
         #time.sleep(5)
 
         driver.find_element_by_xpath("//*[@data-dismiss='modal']").click()
-        #driver.implicitly_wait(10)
-        driver.find_element_by_xpath("//*[@class='far fa-calendar-check']").click()
+        #print("登录2成功")
+        driver.implicitly_wait(10)
+        driver.find_element_by_xpath("//*[@onclick='checkin()']").click()
+        #print("签到成功")
     except:
         raise
     finally:
