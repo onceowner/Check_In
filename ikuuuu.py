942506f32e31044c28ad75856e56d92bc28e2155
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-26 02:53:24 +0800

    Update ikuuuu.py

--- ikuuuu.py
+++ ikuuuu.py
@@ -21,7 +21,7 @@ def ikuuuu():
         driver.find_element_by_xpath("//*[@class='btn btn-primary btn-lg btn-block login']").click()
         #print("登录成功")
         driver.get("https://ikuuu.co/user")
-        print("加载成功")
+        
         #requests.post("https://ikuuu.co/user/checkin")
         #print("提交成功")
         #driver.implicitly_wait(10)
@@ -31,12 +31,14 @@ def ikuuuu():
         #driver.find_element_by_xpath("//*/ul[@data-dismiss='modal']/button[text()='Read']").click()
         #print("登录2成功")
         driver.implicitly_wait(10)
+        print("ikuu前置成功")
         driver.find_element_by_xpath("//*[@onclick='checkin()']").click()
-        #print("签到成功")
+        #print("ikuu签到成功")
     except:
         raise
     finally:
         driver.quit()
+        exit()
 
 if __name__ == '__main__':
     ikuuuu()
