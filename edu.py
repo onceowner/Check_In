0afd47d1d5ef77308d1cb1ad5643819e79c6da3c
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-26 03:50:46 +0800

    Update edu.py

--- edu.py
+++ edu.py
@@ -21,7 +21,7 @@ def edu():
         print('edu刷新成功')
         #time.sleep(5)
         #driver.find_element_by_xpath("//*[@class='waves-attach waves-effect collapsed']").click()
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(10) 
         print('edu前置成功')
         #driver.switch_to.frame(driver.find_element_by_xpath("//button[@class='btn btn-brand btn-flat']"))
         #driver.find_element_by_xpath("//*/span[@class='icon']/span]").click()
