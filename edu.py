3e851f648db5b588f5a7ee9b3decdb89a45cf540
onceowner <49833282+onceowner@users.noreply.github.com>
2022-04-26 03:34:42 +0800

    Update edu.py

--- edu.py
+++ edu.py
@@ -17,17 +17,17 @@ def edu():
         driver.find_element_by_xpath("//*[@id='passwd']").send_keys(password)
         driver.find_element_by_xpath("//*[@id='login']").click()
         #print('登录成功')
-        #driver.get("https://wangzi.uk/user")
-        #print('刷新成功')
+        driver.get("https://wangzi.uk/user")
+        print('edu刷新成功')
         #time.sleep(5)
         #driver.find_element_by_xpath("//*[@class='waves-attach waves-effect collapsed']").click()
         driver.implicitly_wait(10)
-        print('延迟成功')
+        print('edu前置成功')
         #driver.switch_to.frame(driver.find_element_by_xpath("//button[@class='btn btn-brand btn-flat']"))
         #driver.find_element_by_xpath("//*/span[@class='icon']/span]").click()
         #driver.find_element_by_xpath("//*[@id='checkin']").click()
         driver.find_element_by_xpath("//button[contains(text(),'或者摇动手机签到')]")
-        print('签到成功')
+        print('edu签到成功')
     except:
         raise
     finally:
